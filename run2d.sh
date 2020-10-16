c57d749322c6e7e5e02570a79e1bcd201ba33e13
math-vrn <vnikitin90@gmail.com>
2020-10-16 18:12:31 -0500

    figures for the paper

--- run2d.sh
+++ run2d.sh
@@ -1,7 +1,9 @@
-python fsc2d.py \
-/local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_0_30.0/results_admm/u/r_01800.tiff \
-/local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_1_30.0/results_admm/u/r_01800.tiff \
-frc1800.png \
+for k in {1670..1740..2}; do
+python fsc2d_plot_paper.py \
+/local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_0_30.0/results_admm/u/r_0"$k".tiff \
+/local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_1_30.0/results_admm/u/r_0"$k".tiff \
+frc"$k".png \
 1024 \
 5.2 \
-0.0707
+0.055 &
+done
--- run3d.sh
+++ run3d.sh
@@ -1,8 +1,8 @@
-python fsc3d.py \
+python fsc3d_plot_paper.py \
 /local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_0_30.0/results_admm/u/r_00000.tiff \
 /local/data/vnikitin/Kenan_ZP_8keV_interlaced_5000prj_3s_001/dense_0_1_30.0/results_admm/u/r_00000.tiff \
-frc3d.png \
-1024 \
+fsc3d \
+256 \
 5.2 \
 0.0707 \
-1144
+1600
